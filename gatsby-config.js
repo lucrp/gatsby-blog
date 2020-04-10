7d539c5e50f4734cac93fc378fd4789e281b83c4
Lucas Ramos Paiva <lcsrpaiva@gmail.com>
2020-04-10 14:27:00 +0200

    Gatsby config with initial posts

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    title: `ramosweb.dev`
+    description: `This is my personal blog where I write about coding stuff, technologies I appreciate and use and what I'm learning`
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`,  `.md`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`,
+      },
+    },
+  ],
+};
